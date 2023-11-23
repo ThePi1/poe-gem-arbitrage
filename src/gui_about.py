da545a62d5b15f91a62eccaa33d2fa163242c257
Joey Place <calculusman99@gmail.com>
2023-11-23 02:23:52 -0600

    GUI tweaks

--- src/gui_about.py
+++ src/gui_about.py
@@ -12,14 +12,14 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 class Ui_AboutMenu(object):
     def setupUi(self, AboutMenu):
         AboutMenu.setObjectName("AboutMenu")
-        AboutMenu.resize(300, 120)
-        AboutMenu.setMinimumSize(QtCore.QSize(300, 120))
-        AboutMenu.setMaximumSize(QtCore.QSize(300, 120))
+        AboutMenu.resize(400, 135)
+        AboutMenu.setMinimumSize(QtCore.QSize(0, 0))
+        AboutMenu.setMaximumSize(QtCore.QSize(400, 135))
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("data/icon.ico"), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off)
         AboutMenu.setWindowIcon(icon)
         self.label = QtWidgets.QLabel(parent=AboutMenu)
-        self.label.setGeometry(QtCore.QRect(10, 10, 280, 110))
+        self.label.setGeometry(QtCore.QRect(10, 10, 381, 131))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Fixed, QtWidgets.QSizePolicy.Policy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -38,4 +38,4 @@ class Ui_AboutMenu(object):
     def retranslateUi(self, AboutMenu):
         _translate = QtCore.QCoreApplication.translate
         AboutMenu.setWindowTitle(_translate("AboutMenu", "About"))
-        self.label.setText(_translate("AboutMenu", "<html><head/><body><p>Made with ♥ by ThePi</p><p>V_CUR</p><p><a href=\"SRC_URL\"><span style=\" text-decoration: underline; color:#0000ff;\">SRC_URL</span></a></p></body></html>"))
+        self.label.setText(_translate("AboutMenu", "<html><head/><body><p>Made with ♥ by ThePi</p><p>V_CUR</p><p><a href=\"SRC_URL\"><span style=\" text-decoration: underline; color:#0000ff;\">SRC_URL</span></a></p><p>Please open an issue on GitHub for issues / suggestions!</p></body></html>"))
